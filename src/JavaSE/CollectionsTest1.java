e3c742487a12c4670221e7f78cb769a40b9b881a
Wangrui <12345678a@>
2020-03-09 17:52:19 +0800

    Collections知识更新1

--- src/JavaSE/CollectionsTest1.java
+++ src/JavaSE/CollectionsTest1.java
@@ -2,14 +2,12 @@ package JavaSE;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class CollectionsTest1 {
 
     @Test
-    public void test1() {
+    public void test01() {
         ArrayList<Integer> arrayList1 = new ArrayList<>();
         arrayList1.add(-1);
         arrayList1.add(3);
@@ -62,8 +60,9 @@ public class CollectionsTest1 {
         System.out.println(arrayList1);
     }
 
+
     @Test
-    public void test2() {
+    public void test02() {
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
         arrayList.add(-1);
         arrayList.add(3);
@@ -106,5 +105,150 @@ public class CollectionsTest1 {
         System.out.println(Collections.binarySearch(arrayList, 7));
 
     }
+
+
+    @Test
+    public void test03() {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(-1);
+        arrayList.add(3);
+        arrayList.add(3);
+        arrayList.add(-5);
+        arrayList.add(7);
+        arrayList.add(4);
+        arrayList.add(-9);
+        arrayList.add(-7);
+        HashSet<Integer> integers1 = new HashSet<>();
+        integers1.add(1);
+        integers1.add(3);
+        integers1.add(2);
+        Map scores = new HashMap();
+        scores.put("语文", 80);
+        scores.put("Java", 82);
+
+        //Collections.emptyXXX();创建一个空的、不可改变的XXX对象
+        List<Object> list = Collections.emptyList();
+        System.out.println(list);//[]
+        Set<Object> objects = Collections.emptySet();
+        System.out.println(objects);//[]
+        Map<Object, Object> objectObjectMap = Collections.emptyMap();
+        System.out.println(objectObjectMap);//{}
+
+        //Collections.singletonXXX();
+        List<ArrayList<Integer>> arrayLists = Collections.singletonList(arrayList);
+        System.out.println(arrayLists);//[[-1, 3, 3, -5, 7, 4, -9, -7]]
+        //创建一个只有一个元素，且不可改变的Set对象
+        Set<ArrayList<Integer>> singleton = Collections.singleton(arrayList);
+        System.out.println(singleton);//[[-1, 3, 3, -5, 7, 4, -9, -7]]
+        Map<String, String> nihao = Collections.singletonMap("1", "nihao");
+        System.out.println(nihao);//{1=nihao}
+
+        //unmodifiableXXX();创建普通XXX对象对应的不可变版本
+        List<Integer> integers = Collections.unmodifiableList(arrayList);
+        System.out.println(integers);//[-1, 3, 3, -5, 7, 4, -9, -7]
+        Set<Integer> integers2 = Collections.unmodifiableSet(integers1);
+        System.out.println(integers2);//[1, 2, 3]
+        Map<Object, Object> objectObjectMap2 = Collections.unmodifiableMap(scores);
+        System.out.println(objectObjectMap2);//{Java=82, 语文=80}
+
+        //添加出现异常：java.lang.UnsupportedOperationException
+//        list.add(1);
+//        arrayLists.add(arrayList);
+//        integers.add(1);
+    }
+
+    /**
+     * 排序 : sort()
+     */
+    @Test
+    public void test04() {
+        // *************排序 sort****************
+        int a[] = {1, 3, 2, 7, 6, 5, 4, 9};
+        // sort(int[] a)方法按照数字顺序排列指定的数组。
+        Arrays.sort(a);
+        System.out.println("Arrays.sort(a):");
+        for (int i : a) {
+            System.out.print(i);
+        }
+        // 换行
+        System.out.println();
+
+        // sort(int[] a,int fromIndex,int toIndex)按升序排列数组的指定范围
+        int b[] = {1, 3, 2, 7, 6, 5, 4, 9};
+        Arrays.sort(b, 2, 6);
+        System.out.println("Arrays.sort(b, 2, 6):");
+        for (int i : b) {
+            System.out.print(i);
+        }
+        // 换行
+        System.out.println();
+
+        int c[] = {1, 3, 2, 7, 6, 5, 4, 9};
+        // parallelSort(int[] a) 按照数字顺序排列指定的数组(并行的)。同sort方法一样也有按范围的排序
+        Arrays.parallelSort(c);
+        System.out.println("Arrays.parallelSort(c)：");
+        for (int i : c) {
+            System.out.print(i);
+        }
+        // 换行
+        System.out.println();
+
+        // parallelSort给字符数组排序，sort也可以
+        char d[] = {'a', 'f', 'b', 'c', 'e', 'A', 'C', 'B'};
+        Arrays.parallelSort(d);
+        System.out.println("Arrays.parallelSort(d)：");
+        for (char d2 : d) {
+            System.out.print(d2);
+        }
+        // 换行
+        System.out.println();
+    }
+
+
+    /*在做算法面试题的时候，我们还可能会经常遇到对字符串排序的情况,Arrays.sort() 对每个字符串的特定位置进行比较，然后按照升序排序。*/
+    @Test
+    public void test05() {
+        String[] strs = {"abcdehg", "abcdefg", "abcdeag"};
+        Arrays.sort(strs);
+        System.out.println(Arrays.toString(strs));//[abcdeag, abcdefg, abcdehg]
+    }
+
+    /**
+     * 查找 : binarySearch()
+     */
+    @Test
+    public void test06() {
+        // *************查找 binarySearch()****************
+        char[] e = {'a', 'f', 'b', 'c', 'e', 'A', 'C', 'B'};
+        // 排序后再进行二分查找，否则找不到
+        Arrays.sort(e);
+        System.out.println("Arrays.sort(e)" + Arrays.toString(e));
+        System.out.println("Arrays.binarySearch(e, 'c')：");
+        int s = Arrays.binarySearch(e, 'c');
+        System.out.println("字符c在数组的位置：" + s);
+    }
+
+    /**
+     * 比较: equals()
+     */
+    @Test
+    public void test07(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
