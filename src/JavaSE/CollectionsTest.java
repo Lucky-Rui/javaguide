fab0bbc583974dc22a19f7fd5a408012b1e7f48d
Wangrui <12345678a@>
2020-03-10 15:45:42 +0800

    update-Collections工具类和Arrays工具类常见方法总结

--- src/JavaSE/CollectionsTest.java
+++ src/JavaSE/CollectionsTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.util.*;
 
-public class CollectionsTest1 {
+public class CollectionsTest {
 
     @Test
     public void test01() {
@@ -152,9 +152,9 @@ public class CollectionsTest1 {
         System.out.println(objectObjectMap2);//{Java=82, 语文=80}
 
         //添加出现异常：java.lang.UnsupportedOperationException
-//        list.add(1);
-//        arrayLists.add(arrayList);
-//        integers.add(1);
+        //list.add(1);
+        //arrayLists.add(arrayList);
+        //integers.add(1);
     }
 
     /**
@@ -232,23 +232,101 @@ public class CollectionsTest1 {
      * 比较: equals()
      */
     @Test
-    public void test07(){
-
+    public void test07() {
+        // *************比较 equals****************
+        char[] e = {'a', 'f', 'b', 'c', 'e', 'A', 'C', 'B'};
+        char[] f = {'a', 'f', 'b', 'c', 'e', 'A', 'C', 'B'};
+        /*
+         * 元素数量相同，并且相同位置的元素相同。 另外，如果两个数组引用都是null，则它们被认为是相等的 。
+         */
+        // 输出true
+        System.out.println("Arrays.equals(e, f):" + Arrays.equals(e, f));
     }
 
 
+    /**
+     * 填充 : fill()
+     */
+    @Test
+    public void test08() {
+        // *************填充fill(批量初始化)****************
+        int[] g = {1, 2, 3, 3, 3, 3, 6, 6, 6};
+        // 数组中所有元素重新分配值
+        Arrays.fill(g, 3);
+        System.out.println("Arrays.fill(g, 3)：");
+        // 输出结果：333333333
+        for (int i : g) {
+            System.out.print(i);
+        }
+        // 换行
+        System.out.println();
+
+        int[] h = {1, 2, 3, 3, 3, 3, 6, 6, 6,};
+        // 数组中指定范围元素重新分配值
+        Arrays.fill(h, 0, 2, 9);
+        System.out.println("Arrays.fill(h, 0, 2, 9);：");
+        // 输出结果：993333666
+        for (int i : h) {
+            System.out.print(i);
+        }
+    }
+
+    /**
+     * 转列表 asList()
+     */
+    @Test
+    public void test09() {
+        // *************转列表 asList()****************
+        /*
+         * 返回由指定数组支持的固定大小的列表。
+         * （将返回的列表更改为“写入数组”。）该方法作为基于数组和基于集合的API之间的桥梁，与Collection.toArray()相结合 。
+         * 返回的列表是可序列化的，并实现RandomAccess 。
+         * 此方法还提供了一种方便的方式来创建一个初始化为包含几个元素的固定大小的列表如下：
+         */
+        List<String> stooges = Arrays.asList("Larry", "Moe", "Curly");
+        System.out.println(stooges);
+    }
 
 
+    /**
+     * 转字符串 toString()
+     */
+    @Test
+    public void test10() {
+        // *************转字符串 toString()****************
+        /*
+         * 返回指定数组的内容的字符串表示形式。
+         */
+        char[] k = {'a', 'f', 'b', 'c', 'e', 'A', 'C', 'B'};
+        System.out.println(Arrays.toString(k));// [a, f, b, c, e, A, C, B]
+    }
 
-
-
-
-
-
-
-
-
-
+    /**
+     * 复制 copyOf()
+     */
+    @Test
+    public void test11() {
+        // *************复制 copy****************
+        // copyOf 方法实现数组复制,h为数组，6为复制的长度
+        int[] h = {1, 2, 3, 3, 3, 3, 6, 6, 6,};
+        int i[] = Arrays.copyOf(h, 6);
+        System.out.println("Arrays.copyOf(h, 6);：");
+        // 输出结果：123333
+        for (int j : i) {
+            System.out.print(j);
+        }
+        // 换行
+        System.out.println();
+        // copyOfRange将指定数组的指定范围复制到新数组中
+        int j[] = Arrays.copyOfRange(h, 6, 11);
+        System.out.println("Arrays.copyOfRange(h, 6, 11)：");
+        // 输出结果66600(h数组只有9个元素这里是从索引6到索引11复制所以不足的就为0)
+        for (int j2 : j) {
+            System.out.print(j2);
+        }
+        // 换行
+        System.out.println();
+    }
 
 
 }
